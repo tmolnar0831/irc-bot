73289880aa9427b1c847a07f88256253cebb11f5
Tamas Molnar <tmolnar0831@gmail.com>
2019-01-11 18:09:51 +0100

    Addig some form of the things

--- main.lisp
+++ main.lisp
@@ -19,6 +19,9 @@
 
 ;;; Actions
 (defun msg-hook (message)
-  (format t "COMMAND: ~S~%" (last (cl-irc:arguments message))))
+  (let ((channel (first (irc:arguments message)))
+        (arguments (last (irc:arguments message)))
+        (source (irc:source message)))
+    (irc:privmsg *connection* *channel* (format nil "~S" arguments))))
 
 (irc:add-hook *connection* 'irc:irc-privmsg-message 'msg-hook)
