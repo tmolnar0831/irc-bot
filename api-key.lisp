61762c9a92e1e38c9cdd636f645156efca438738
Pascal J. Bourguignon <pjb@informatimago.com>
2019-01-13 19:16:43 +0100

    Added api-key.lisp, loading the keys from ~/.config/*.apikey

---
+++ api-key.lisp
@@ -0,0 +1,6 @@
+
+(defun load-api-key (service-name)
+  (let ((path (merge-pathnames (make-pathname :name service-name :type "apikey")
+                               (uiop/configuration:xdg-config-home) nil)))
+    (with-open-file (in path :direction :input)
+      (string-trim #(#\space #\tab) (read-line in)))))
